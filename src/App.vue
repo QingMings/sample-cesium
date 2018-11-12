bf68e87838c8e804a321fcab17387c36a801c66d
QingMings <1821063757@qq.com>
2018-11-12 21:30:50 +0800

    sample-cesium init

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
+  html, body, #cesiumContainer {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/views/sample-cesium.vue
@@ -0,0 +1,22 @@
+<style lang="less">
+@import "sample-cesium";
+</style>
+<template>
+    <div id="cesiumContainer">
+
+    </div>
+</template>
+
+<script>
+import Cesium from 'cesium/Cesium'
+import  'cesium/Widgets/widgets.css'
+import iezNavi from 'iez-navi/viewerCesiumNavigationMixin'
+export default {
+  name: "sample-cesium",
+  mounted () {
+    let viewer = new Cesium.Viewer('cesiumContainer');
+    iezNavi(viewer,{});
+  }
+}
+</script>
+
